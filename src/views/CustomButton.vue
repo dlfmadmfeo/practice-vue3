d28f883372071c87067d4617595cebd8f39f53c2
junhee <dlfmadmfeo@gmail.com>
2024-08-24 17:03:08 +0900

    init

---
+++ src/views/CustomButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps(["title"])
+const emit = defineEmits(["update:title"]);
+</script>
+
+<template>
+    <input type="text" :value="props.title" @input="(e: any): void => emit('update:title', e.target?.value)">
+    <!-- <button @click="emit('increment')">Click</button> -->
+</template>
